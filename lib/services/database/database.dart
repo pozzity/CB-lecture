01f37ec50fc07407617614d49dbbc248c0504ea0
nidovic <feutseludovic@gmail.com>
2021-09-13 19:42:04 +0200

    delete file database_helper.dart

--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -1,4 +1,12 @@
-part of database_helper;
+library database_helper;
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+part 'src/sqlite/sqlite_implemetation.dart';
+part 'src/sqlite/sqlite_table.dart';
+part 'src/sqlite/sqlite_colum.dart';
+part 'src/test_implemetation.dart';
 
 /// Helpers class that help us to retrieve, edit or delete document or
 /// collection from our database.
